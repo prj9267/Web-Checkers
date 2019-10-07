415221651d931cc0b4463fd0fbcd51f0d1c0bdf0
Mike Cao <mc2522@rit.edu>
2019-10-07 15:47:18 -0400

    need to finish getgameroute. Lobby has something wrong as well. Check sign in.

--- src/main/java/com/webcheckers/ui/GetGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -9,13 +9,10 @@ import java.util.Objects;
 
 public class GetGameRoute implements Route {
     // Values used in the view-model map for rendering the game view.
-    static final String VIEW_NAME = "game.ftl";
-    static final String TITLE = "Let's play a game...";
+    private static final String VIEW_NAME = "game.ftl";
+    private static final String TITLE_ATTR = "title";
+    private static final String TITLE = "Let's play a game...";
     private final TemplateEngine templateEngine;
-
-    //TODO temp player
-    private Player player;
-
 
     /**
      * The constructor for the {@code GET /game} route handler.
@@ -24,13 +21,9 @@ public class GetGameRoute implements Route {
      *    The {@link TemplateEngine} used for rendering page HTML.
      */
     public GetGameRoute(final TemplateEngine templateEngine){
-        // validation
-
         Objects.requireNonNull(templateEngine, "templateEngine must not be null");
         this.templateEngine = templateEngine;
-        this.player = new Player("Name");
     }
-
 
     /**
      * {@inheritDoc}
@@ -38,10 +31,10 @@ public class GetGameRoute implements Route {
     @Override
     public Object handle(Request request, Response response){
         // no session for now
-
+        //todo UNCOMPLETE
         // build the View-Model
         final Map<String, Object> vm = new HashMap<>();
-        vm.put(GetHomeRoute.TITLE_ATTR, TITLE);
+        vm.put(TITLE_ATTR, TITLE);
         return templateEngine.render(new ModelAndView(vm, VIEW_NAME));
     }
 }
