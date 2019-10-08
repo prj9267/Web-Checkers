3ef4ef94a95ed2156cdadd1953827ced3f434282
Parker Johnson <prj9267@rit.edu>
2019-10-08 14:32:27 -0400

    Added Sign Up view and minor changes to home view

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -2,6 +2,7 @@ package com.webcheckers.model;
 
 public class Player {
     private String name;
+    private String password;
 
     public Player(String name){
         this.name = name;
@@ -9,5 +10,17 @@ public class Player {
 
     public String getName(){
         return name;
+    }
+
+    public String getPassword(){ return password; }
+
+    @Override
+    public boolean equals(Object o){
+        if(o instanceof Player){
+            if(((Player) o).name == name && ((Player) o).password == password){
+                return true;
+            }
+        }
+        return false;
     }
 }
