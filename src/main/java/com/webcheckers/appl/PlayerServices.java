b818deb4ce9e73b3015a6972bb09b106e529e49b
Mike Cao <mc2522@rit.edu>
2019-10-13 20:19:07 -0400

    Working on a map that checks if the username is valid and adds them if valid.

--- src/main/java/com/webcheckers/appl/PlayerServices.java
+++ src/main/java/com/webcheckers/appl/PlayerServices.java
@@ -1,12 +1,17 @@
 package com.webcheckers.appl;
 
 import com.webcheckers.model.Match;
+import com.webcheckers.model.Player;
+
+import java.util.Map;
 
 
 public class PlayerServices {
 
     //Attributes
     private Match match;
+    //playerList holding the names of players
+    private static Map<String, Player> playerList;
     private final GameCenter gameCenter;
 
     /**
@@ -21,7 +26,9 @@ public class PlayerServices {
     /**
      * End the current match;
      */
-    public void endSession(){ match = null; }
+    public void endSession() {
+        match = null;
+    }
 
     /**
      * Creates a new match between players.
@@ -31,5 +38,45 @@ public class PlayerServices {
      */
     public Match getMatch(String player1, String player2){
         return gameCenter.getMatch(player1, player2);
+    }
+
+    /**
+     * Checks if the username is taken.
+     * @param username  - the user
+     * @return          - true if not taken and false if taken
+     */
+    public boolean isTaken(String username) {
+        return !playerList.containsKey(username);
+    }
+
+    /**
+     * Checks if the registered name contains only alphanumeric characters.
+     * @param username  - the user
+     * @return          - true if name is valid and false if not
+     */
+    public boolean isAlphanumeric(String username) {
+        return username.matches("[a-zA-Z_0-9]+([a-zA-Z_0-9]|\\s)*$");
+    }
+
+    /**
+     * Checks if the username is valid
+     * @param username  - the user
+     * @return          - true if valid and false if invalid
+     */
+    public boolean isValid(String username) {
+        return isAlphanumeric(username) && isTaken(username);
+    }
+
+    /**
+     * Adds the username and creates a new Player if the username is valid.
+     * @param username  - the user
+     * @return          - true if the player has been added and false if not
+     */
+    public synchronized boolean confirmation(String username) {
+        if(isValid(username)) {
+            playerList.put(username, new Player(username));
+            return true;
+        }
+        return false;
     }
 }
