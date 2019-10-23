ac9d6366dd2d29549d302580f908e9a6fed3c462
Mike Cao <mc2522@rit.edu>
2019-10-23 01:36:16 -0400

    Created more Player tests that tests for equality, invalid name, illegal constructor argument.

--- src/test/java/com/webcheckers/model/PlayerTest.java
+++ src/test/java/com/webcheckers/model/PlayerTest.java
@@ -3,6 +3,8 @@ package com.webcheckers.model;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.util.MissingFormatArgumentException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @Tag("Model-tier")
@@ -14,5 +16,28 @@ public class PlayerTest {
     public void checkEquality() {
         Player player2 = new Player(name);
         assertTrue(player.equals(player2), "Players are not equal");
+    }
+
+    @Test
+    public void checkSelf() {
+        assertTrue(player.equals(player));
+    }
+
+    @Test
+    public void checkHash() {
+        Player player2 = new Player(name);
+        assertEquals(player.getName().hashCode(), player2.getName().hashCode(), "Hashcode are not equal.");
+    }
+
+    @Test
+    public void constructorNullArg() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Player(null); }, "Player with null name allowed.");
+    }
+
+    @Test
+    public void testInvalidName() {
+        Player player2 = new Player("!TrackStaR***美国");
+        assertFalse(player2.containsInvalidCharacter(), "Invalid characters are accepted.");
     }
 }
