a4adc72aee4c43ec023748ec21ce179e3821fbc7
Mike Cao <mc2522@rit.edu>
2019-10-23 01:03:20 -0400

    Created Player Test, starting tests.

---
+++ src/test/java/com/webcheckers/model/PlayerTest.java
@@ -0,0 +1,16 @@
+package com.webcheckers.model;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Tag("Model-tier")
+public class PlayerTest {
+    private static final String INVALID_USERNAME = "#goJackson *";
+
+    @Test
+    public void checkInvalidUsername() {
+        
+    }
+}
