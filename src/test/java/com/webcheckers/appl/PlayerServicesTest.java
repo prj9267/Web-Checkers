aef4f5b7bbcbcac18355e2de167e18ad2dedd209
egc2846 <egc2846@rit.edu>
2019-10-30 17:49:46 -0400

    PlayerServicesTest updated

--- src/test/java/com/webcheckers/appl/PlayerServicesTest.java
+++ src/test/java/com/webcheckers/appl/PlayerServicesTest.java
@@ -18,34 +18,66 @@ public class PlayerServicesTest {
     private PlayerServices CuT;
 
     @BeforeEach
-    public void setup() {
+    void setup() {
         CuT = new PlayerServices();
     }
 
     @Test
-    public void checkGetPlayerList() {
+    void checkGetPlayerList() {
         playerList.add(playerOne);
-
         CuT.addPlayer(playerOne);
-
         assertEquals(playerList, CuT.getPlayerList(), "PLayerList not updated properly");
     }
 
     @Test
-    public void checkAddPlayer() {
+    void checkAddPlayer() {
         playerList.add(playerOne);
-
         CuT.addPlayer(playerOne);
-
-        assertEquals(playerList, CuT.getPlayerList(), "Player not added to list properly");
-
+        assertEquals(playerList, CuT.getPlayerList(), "PlayerOne not added to list properly");
+        playerList.add(playerTwo);
+        CuT.addPlayer(playerTwo);
+        assertEquals(playerList, CuT.getPlayerList(), "PlayerTwo not added to list properly");
     }
 
+    @Test
+    void checkPlayerService(){
+        PlayerServices playerSrv = new PlayerServices();
+        assertEquals(playerSrv.getPlayerList(), CuT.getPlayerList(), "PlayerServices Built incorrectly");
+    }
 
     @Test
-    public void checkPlayerService(){
-        PlayerServices playerSrv = new PlayerServices();
+    void checkGetPlayer() {
+        CuT.addPlayer(playerOne);
+        assertEquals(playerOne, CuT.getPlayer("One"), "PlayerOne not gotten correctly");
+        assertNull(CuT.getPlayer("Two"), "PlayerTwo found but not added");
+    }
 
-        assertEquals(playerSrv.getPlayerList(), CuT.getPlayerList(), "PlayerServices Built incorrectly");
+    @Test
+    void checkRemovePlayers() {
+        CuT.addPlayer(playerOne);
+        CuT.addPlayer(playerTwo);
+        playerList.add(playerOne);
+        playerList.add(playerTwo);
+        assertEquals(playerList, CuT.getPlayerList(), "Player list not constructed correctly");
+
+        CuT.removePlayer(playerOne);
+        playerList.remove(playerOne);
+        assertEquals(playerList, CuT.getPlayerList(), "PlayerOne not removed correctly");
+
+        CuT.removePlayer(playerTwo);
+        assertNotEquals(playerList, CuT.getPlayerList(), "PlayerTwo still in player list");
+    }
+
+    @Test
+    void checkNumPlayers() {
+        assertEquals(0, CuT.numPlayers(), "Player list is not size 0");
+        CuT.addPlayer(playerOne);
+        assertEquals(1, CuT.numPlayers(), "Player list is not size 1");
+        CuT.addPlayer(playerTwo);
+        assertEquals(2, CuT.numPlayers(), "Player list is not size 2");
+        CuT.removePlayer(playerTwo);
+        assertEquals(1, CuT.numPlayers(), "Player list is not size 1");
+        CuT.removePlayer(playerOne);
+        assertEquals(0, CuT.numPlayers(), "Player list is not size 0");
     }
 }
